9d972fe8c59faca6acf287698ed62c907f7ae421
6rube <47418292+6rube@users.noreply.github.com>
2022-09-13 12:40:19 +0000

    add better background and parsing

--- assets/js/fav-flash.js
+++ assets/js/fav-flash.js
@@ -4,16 +4,16 @@ function set_favicon() {
     // Assign a constant variable and get them by the favicon Id
     const favicon = document.querySelector('[rel=icon]');
     if(pos == 0) {
-        favicon.href = "assets/ico/1.png";
+        favicon.href = "/assets/ico/1.png";
     }
     if(pos == 1) {
-        favicon.href = "assets/ico/2.png";
+        favicon.href = "/assets/ico/2.png";
     }
     if(pos == 2) {
-        favicon.href = "assets/ico/3.png";
+        favicon.href = "/assets/ico/3.png";
     }
     if(pos == 3) {
-        favicon.href = "assets/ico/4.png";
+        favicon.href = "/assets/ico/4.png";
         pos = -1
     }
     pos++
